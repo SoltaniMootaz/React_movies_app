00edc9f129648c4da31d0a656fde43d6456ef40a
Mootaz <mootaz.1.soltani2019@gmail.com>
2022-04-10 21:03:55 +0100

    prototype v0.1

---
+++ client/src/Components/Homesection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import Moviecard from './Moviecard'
+
+function Homesection() {
+  return (
+    <React.Fragment>
+       
+      <Container fluid>
+          <Row >
+              <Col>
+          <h3 style={{color:"white",fontWeight:'bold'}}>Trending</h3> 
+          </Col>
+          </Row>
+          <Row>
+            <Col><Moviecard /></Col>
+            <Col><Moviecard /></Col>
+            <Col><Moviecard /></Col>
+          </Row>
+      </Container>
+      <hr style={{border:'0.08rem solid #4E9F3D',backgroundColor:'#4E9F3D'}} />
+    </React.Fragment>
+  )
+}
+
+export default Homesection
